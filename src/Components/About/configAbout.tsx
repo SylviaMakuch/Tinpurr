d68506ce3cc63394ee0eba010b1fc3a63806c52e
Sylvia Makuch <sylvia.makuch@protonmail.com>
2021-12-10 18:04:33 -0800

    configabout added

---
+++ src/Components/About/configAbout.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styled from "styled-components";
+import checkmark from "./../mediacheckmark.png";
+import paw from "./../media/paw.png";
+import like from "./../media/like.png";
+
--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -5,22 +5,24 @@ import phone from "./media/phone.svg";
 const BackGround = styled.div`
     background-color: #F35D6F;
     display: flex;
+    align-items: center;
+    justify-content: center;
 `
 const MobileImg = styled.img`
     
 `
 const Slogan = styled.div`
     color: white;
-    font-family: Work Sans;
-    
+    font-family: 'Work Sans', sans-serif;
+    font-size: 70px;
+    font-weight: 800;
 `
 
 export default function Hero() {
     return (
         <BackGround>
             <MobileImg src={phone} />
-            <Slogan> Share Tuna with a Kitty Nearby !</Slogan>
+            <Slogan> Share Tuna with a Kitty Nearby!</Slogan>
         </BackGround>
-
     )
 };
