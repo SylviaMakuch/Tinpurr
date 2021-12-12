2ff1bfe07564e1a10780597633663f75628a23ed
Sylvia Makuch <sylvia.makuch@protonmail.com>
2021-12-11 20:45:48 -0800

    added slider, pulling the container instead

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./Components/Hero/Hero";
 import About from "./Components/About/About";
 import CarouselContainer from "./Components/Carousel/CarouselContainer";
+import SimpleSlider from "./Components/Carousel/Slider";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <CarouselContainer />
+      <SimpleSlider />
     </div>
   );
 }
--- src/Components/Carousel/Slider.tsx
+++ src/Components/Carousel/Slider.tsx
@@ -1,30 +1,22 @@
 import React from "react";
 import Slider from "react-slick";
+import CarouselContainer from "./CarouselContainer";
+import CarouselCard from "./CarouselCard";
+import { carouselData } from "./configCarousel";
 
-class ReactSlickDemo extends React.Component {
-  render() {
-    var settings = {
-      dots: true
-    };
-    return (
-      <div className="container">
-        <Slider {...settings}>
-          <div>
-            <img src="http://placekitten.com/g/400/200" />
-          </div>
-          <div>
-            <img src="http://placekitten.com/g/400/200" />
-          </div>
-          <div>
-            <img src="http://placekitten.com/g/400/200" />
-          </div>
-          <div>
-            <img src="http://placekitten.com/g/400/200" />
-          </div>
-        </Slider>
-      </div>
-    );
-  }
+export default function SimpleSlider() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  };
+  return (
+    <Slider {...settings}>
+             {carouselData.map(({image, title, text}, index)=>{
+                 return <CarouselCard image={image} title={title} text={text} />
+             })}
+    </Slider>
+  );
 }
-
-ReactDOM.render(<ReactSlickDemo />, document.getElementById("container"));
