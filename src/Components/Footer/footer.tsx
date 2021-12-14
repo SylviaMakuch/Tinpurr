23b43de6a7f25ba905371cf8126bb51519fa6135
Sylvia Makuch <sylvia.makuch@protonmail.com>
2021-12-14 10:33:14 -0800

    Footer added

--- src/Components/Footer/footer.tsx
+++ src/Components/Footer/footer.tsx
@@ -1,25 +1,33 @@
 import React from "react";
 import styled from "styled-components";
+import Socials from "./Socials";
+import tinpurrlogo from "./../media/tinpurrlogo.svg";
 
 
 const BackGround = styled.div`
     background-color: #F35D6F;
     display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
     align-items: center;
-    justify-content: center;
     height: 50vh;
 `
-const Slogan = styled.div`
+const Text = styled.div`
     color: white;
     font-family: 'Work Sans', sans-serif;
-    font-size: 70px;
-    font-weight: 800;
+    font-size: 25px;
+    font-weight: 300;
 `
+const Img = styled.img`
+    width: 30%;
+`   
 
 export default function Footer() {
     return (
         <BackGround>
-         
+        <Img src={tinpurrlogo} />
+            <Text> By: Sylvia Makuch</Text>
+            <Socials />
         </BackGround>
     )
 };
