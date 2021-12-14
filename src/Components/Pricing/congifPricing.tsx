ec0c41b24b626e39cac5a593fac50aad895de572
Sylvia Makuch <sylvia.makuch@protonmail.com>
2021-12-14 00:14:06 -0800

    config for pricing

---
+++ src/Components/Pricing/congifPricing.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+import tier1 from "./../media/tier1.svg";
+import tier2 from "./../media/tier2.svg";
+import tier3 from "./../media/tier3.svg";
+
+
+export const pricingData = [
+    {
+        image: tier1,
+        title: "Free",
+        text: "5 Matches Per Day 10 Messages Per Day Unlimited App Usage",
+
+    },
+    {
+        image: tier2,
+        title:"$19.99/month",
+        text:"Unlimited Matches Unlimited Messages Unlimited App Usage",
+    
+    },
+    {   image: tier3,
+        title:"$250/annually",
+        text:"Priority Listing" "Unlimited Matches" "Unlimited Messages" "Unlimited App Usage"
+    },
+];
