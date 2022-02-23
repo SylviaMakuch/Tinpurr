4f5a617d595ae8f2d621d5e7db8d8d76c778223e
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-02-22 21:14:01 -0800

    testimonal removed and added a gui

--- src/Components/Testimonials/Testimontials.tsx
+++ src/Components/Testimonials/Testimontials.tsx
@@ -1,149 +1,56 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { Component } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 import tobi from "./../media/tobi.jpg";
 import girly from "./../media/girly.jpeg"
 import loveshades from "./../media/loveshades.jpg";
-
-
-const Container = styled.div`
-   display: flex;
-   flex-direction: column;
-   align-items: center;
-   margin: 40px auto;
-   max-width: 700px;
-   height: 900px
-   justify-content: center;
-   line-height: 2;
-   @media (max-width: 600px) {
-        margin: 120px 60px;
-    }
-`;
-
-const Img = styled.img`
-    border-radius: 50%;
-    padding: 10px;
-    border: 4px solid linear-gradient(45deg,#FE6B8B 30%, #FF8E53 90%);
-    height: 350px;
-    width: 350px;
-    background:linear-gradient(45deg,#FE6B8B 30%, #FF8E53 90%); 
-    @media(max-width: 800px) {
-        height: 300px;
-        width: 300px;
-`;
-
-const Title = styled.div`
-    color: #000000;
-    text-align: center;
-    font-size: 40px;
-    font-family: 'Fuzzy Bubbles';
-    font-weight: 500;
-    margin: 60px;
-    font-style: italic;
-    @media(max-width: 1100px) {
-        font-size: 35px;
-        margin: 30px;
-    }
-    @media(max-width: 800px) {
-        font-size: 30px;
-        margin: 20px;
-    }
-`;
-
-const Text = styled.div`
-    color: #000000;
-    font-size: 30px;
-    font-family: 'Work Sans', sans-serif;
-    font-weight: 400;
-    margin: 30px;
-    @media(max-width: 1100px) {
-        font-size: 25px;
-        margin: 15px;
-    }
-    @media(max-width: 800px) {
-        font-size: 20px;
-        margin: 15px;
-    }
-`;
-
-const Span = styled.span`
-    height: 30px;
-    width: 30px;
-    margin: 20px;
-    display: flex;
-    align-items: center;
-    justify-content:center;
-    cursor: pointer;
-    float: right;
-    border-radius: 50%;
-    &:before{
-        content: "";
-        border-radius: 50%;
-        height: 6px;
-        width: 6px;
-        background-color: #d4d4d4;
-        transition: background-color 0.3s ease;
-  }
-     &:hover{
-        background-color: #d6d6e05f;
-        border-radius: 50%;
-  }
-
-`;
-interface CarouselCard {
-    image: string;
-    title: string;
-    text: string;
-    name: string;
-};
-
-interface CarouselCardProperties extends Record<number, CarouselCard> {
-};
+import "./TestStyles.css";
 
 export default function Testimonials() {
-    const carouselData: CarouselCardProperties = {
-        0: {
-            image: tobi,
-            title: '"I no longer have to howl at the moon to call for my lady !!"',
-            name: "Tobi ",
-            text: "📍 - Vancouver, Canada",
-        },
-        1: {
-            image: girly,
-            title: '"With Enrico going on dates, we have more time to ourselves!"',
-            name: "Gina",
-            text: " 📍 - Rome, Italy",
-
-        },
-        2: {
-            image: loveshades,
-            title: '"I no longer have to worry about staying clean, I have kitties licking me every night.  I have Love Shades on."',
-            name: " Princess ",
-            text: " 📍 - Georgia, USA",
-        },
-    };
-
-    const [current, setCurrent] = useState(carouselData[0])
-
-    const [active, setActive] = useState(0)
-
-    const handleSetClick = (event: any) => {
-        setActive(event.target.getAttribute(handleSetClick));
-        setCurrent(carouselData[event.target.getAttribute("data-Testimonials")]);
-    };
     return (
-        <Container>
-            <Img src={current.image} />
-            <Title>{current.title}</Title>
-            <Text>{current.name}</Text>
-            <Text>{current.text}</Text>
-            <div>
-                {Object.keys(carouselData).map(index => (
-                    <Span
-                        onClick={(event: any) => handleSetClick(event)}
-                        key={index}
-                    />
-                ))}
-            </div>
-        </Container>
-    )
-}
+      <Carousel
+        showArrows={true}
+        infiniteLoop={true}
+        showThumbs={false}
+        showStatus={false}
+        autoPlay={true}
+        interval={6100}
+      >
+        <div>
+          <img src={tobi} />
+          <div className="myCarousel">
+            <h3>Shirley Fultz</h3>
+            <h4>Designer</h4>
+            <p>
+              It's freeing to be able to catch up on customized news and not be
+              distracted by a social media element on the same site
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <img src={girly} />
+          <div className="myCarousel">
+            <h3>Daniel Keystone</h3>
+            <h4>Designer</h4>
+            <p>
+              The simple and intuitive design makes it easy for me use. I highly
+              recommend Fetch to my peers.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <img src={loveshades} />
+          <div className="myCarousel">
+            <h3>Theo Sorel</h3>
+            <h4>Designer</h4>
+            <p>
+              I enjoy catching up with Fetch on my laptop, or on my phone when
+              I'm on the go!
+            </p>
+          </div>
+        </div>
+      </Carousel>
+    );
+  }
